1ddd57a0a8127654dbfffa9286784fe32f42a576
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-03 19:11:01 +0900

    feat : webhookChaining 라우팅 구현

--- routes/github.api.js
+++ routes/github.api.js
@@ -2,5 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const githubController = require("../controllers/github.controller");
+const autoController = require("../controllers/auth.controller");
+
+router.post(
+  "/webhook/chaining",
+  autoController.findUserByToken,
+  githubController.webhookChaining
+);
 
 module.exports = router;
