4a5be39a9210a7c70f09fac327a70ab04451b651
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-03 19:10:44 +0900

    feat : webhookChaining 기능 구현

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -1,3 +1,54 @@
+const axios = require("axios");
+const User = require("../models/User");
+
 const githubController = {};
+
+githubController.webhookChaining = async (req, res) => {
+  const { repoFullName } = req.body;
+
+  if (!repoFullName) {
+    return res.status(400).json({ message: "repoFullName is required" });
+  }
+
+  let owner;
+  let repo;
+
+  [owner, repo] = repoFullName.split("/");
+  const userId = req.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "GitHub access token not found for this user" });
+  }
+
+  const githubAccessToken = user.githubAccessToken;
+  const githubApiUrl = `https://api.github.com/repos/${owner}/${repo}/hooks`;
+
+  const payload = {
+    name: "web",
+    active: true,
+    event: ["push", "pull_request"],
+    config: {
+      url: "https://unterminative-tawna-sunproof.ngrok-free.dev/api/github/webhook",
+      content_type: "json",
+      secret: "asd",
+    },
+  };
+
+  const githubRes = await axios.post(githubApiUrl, payload, {
+    headers: {
+      Authorization: `Bearer ${githubAccessToken}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+  });
+  console.log("aaa", githubRes);
+  return res.status(201).json({ message: "Webhook created" });
+};
 
 module.exports = githubController;
