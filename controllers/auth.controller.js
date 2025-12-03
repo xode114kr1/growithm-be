08bea240a87b3fcddfe70e62af2cf5259eacbc6a
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-03 18:54:30 +0900

    feat : findUserByToken - access토큰으로 user._id 찾는 함수 구현

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -114,4 +114,28 @@ authController.issueTokensAndRespond = (req, res) => {
   });
 };
 
+authController.findUserByToken = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies?.accessToken;
+
+    if (!accessToken) {
+      return res.status(401).json({ message: "Access token is missing" });
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
+    const userId = decoded.userId;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Invalid token payload" });
+    }
+
+    const user = await User.findById(userId);
+
+    req.userId = user._id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "invalid or expired token" });
+  }
+};
+
 module.exports = authController;
