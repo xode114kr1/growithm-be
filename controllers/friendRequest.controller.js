bef0919b46021a732f30e3884b088cfc82e222bc
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-08 22:19:56 +0900

    fix : 오타 수정

--- controllers/friendRequest.controller.js
+++ controllers/friendRequest.controller.js
@@ -7,10 +7,10 @@ friendRequestController.getReceiveFriendRequsets = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const friends = await FriendRequest.find({ from: userId }).populate("to");
+    const friends = await FriendRequest.find({ to: userId }).populate("to");
 
     if (!friends) return res.status(401).json({ error: "Cannot find friend" });
-    console.log(friends);
+
     return res.status(201).json({ message: "Success", data: friends });
   } catch (error) {
     return res.status(400).json({ error: error });
@@ -21,10 +21,10 @@ friendRequestController.getSendFriendRequsets = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const friends = await FriendRequest.find({ to: userId }).populate("to");
+    const friends = await FriendRequest.find({ from: userId }).populate("to");
 
     if (!friends) return res.status(401).json({ error: "Cannot find friend" });
-    console.log(friends);
+
     return res.status(201).json({ message: "Success", data: friends });
   } catch (error) {
     return res.status(400).json({ error: error });
