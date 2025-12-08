8eb5040494d2cb2733f1d58020b3e677ede14cd7
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-08 21:53:52 +0900

    feat : getSendFriendRequsets 기능 구현

--- controllers/friendRequest.controller.js
+++ controllers/friendRequest.controller.js
@@ -3,11 +3,25 @@ const User = require("../models/User");
 
 const friendRequestController = {};
 
-friendRequestController.getWaitFriendRequsets = async (req, res) => {
+friendRequestController.getReceiveFriendRequsets = async (req, res) => {
   try {
     const userId = req.user._id;
 
     const friends = await FriendRequest.find({ from: userId }).populate("to");
+
+    if (!friends) return res.status(401).json({ error: "Cannot find friend" });
+    console.log(friends);
+    return res.status(201).json({ message: "Success", data: friends });
+  } catch (error) {
+    return res.status(400).json({ error: error });
+  }
+};
+
+friendRequestController.getSendFriendRequsets = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const friends = await FriendRequest.find({ to: userId }).populate("to");
 
     if (!friends) return res.status(401).json({ error: "Cannot find friend" });
     console.log(friends);
--- routes/friendRequest.api.js
+++ routes/friendRequest.api.js
@@ -5,9 +5,15 @@ const authController = require("../controllers/auth.controller");
 const friendRequestController = require("../controllers/friendRequest.controller");
 
 router.get(
-  "/wait",
+  "/send",
   authController.findUserByToken,
-  friendRequestController.getWaitFriendRequsets
+  friendRequestController.getSendFriendRequsets
+);
+
+router.get(
+  "/receive",
+  authController.findUserByToken,
+  friendRequestController.getReceiveFriendRequsets
 );
 
 router.post(
