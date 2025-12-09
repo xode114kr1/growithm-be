5e89f7e8f8e15c40538f8274d647c0053277a20b
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-09 23:52:21 +0900

    feat : init getFriendList

--- controllers/friend.controller.js
+++ controllers/friend.controller.js
@@ -1,3 +1,11 @@
 const friendController = {};
 
+friendController.getFriendList = (req, res) => {
+  try {
+    return res.status(200).json({ message: "success", data: "data" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = friendController;
--- routes/friend.api.js
+++ routes/friend.api.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const friendController = require("../controllers/friend.controller");
+
+router.get("/", friendController.getFriendList);
+
 module.exports = router;
