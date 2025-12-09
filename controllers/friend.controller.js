d51cf26d4d1774acbc77454014d21e388c1c755d
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-09 23:54:44 +0900

    feat : getFriendList 기능 구현

--- controllers/friend.controller.js
+++ controllers/friend.controller.js
@@ -2,7 +2,9 @@ const friendController = {};
 
 friendController.getFriendList = (req, res) => {
   try {
-    return res.status(200).json({ message: "success", data: "data" });
+    const user = req.user;
+
+    return res.status(200).json({ message: "success", data: user.friends });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
--- routes/friend.api.js
+++ routes/friend.api.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const authController = require("../controllers/auth.controller");
 const friendController = require("../controllers/friend.controller");
 
-router.get("/", friendController.getFriendList);
+router.get("/", authController.findUserByToken, friendController.getFriendList);
 
 module.exports = router;
