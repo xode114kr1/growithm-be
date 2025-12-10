9330de16fe623651cd6653236caf035bc82dc362
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-10 21:10:14 +0900

    feat : createStudy 기능 구현

--- controllers/study.controller.js
+++ controllers/study.controller.js
@@ -1,13 +1,28 @@
+const Study = require("../models/Study");
+const StudyRequest = require("../models/StudyRequest");
+
 const studyController = {};
 
 studyController.createStudy = async (req, res) => {
   try {
-    // const userId = req.user._id;
+    const userId = req.user._id;
     const { title, explanation, members } = req.body;
-    console.log(title, explanation, members);
+
+    const study = await Study.create({ title, explanation, owner: userId });
+
+    members?.map(async (item) => {
+      const studyRequest = await StudyRequest.create({
+        studyId: study?._id,
+        userId: item,
+      });
+      if (!studyRequest) {
+        return res.status(404).json({ error: "cannot create study request" });
+      }
+    });
+
     return res.status(200).json({
       message: "Success",
-      data: { title, explanation, members },
+      data: { title, explanation, members, userId },
     });
   } catch (error) {
     return res.status(400).json({ error: error.message });
