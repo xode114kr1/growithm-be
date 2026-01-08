7f7504c0881e01785937517565cba67f59fe86fb
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-01-08 14:27:05 +0900

    fix : 웹훅 receive 특정 problem 저장 못하는 버그 수정
    - try, catch로 감싸 안정성 향상
    - 성공 시 res 201 전송
    - create -> find and update로 안정성 향상

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -56,66 +56,105 @@ githubController.webhookChaining = async (req, res, next) => {
 };
 
 githubController.webhookReceive = async (req, res, next) => {
-  const payload = req.body;
-  const timestamp = payload.head_commit?.timestamp?.slice(0, 10) || null;
-  const repoFullName = payload.repository?.full_name;
-  const commitSha = payload.after;
-  const githubSenderId = payload.sender?.id;
-  const pathList = payload?.head_commit.added;
-  const platform = flatformMap[payload?.head_commit?.added[0].split("/")[0]];
+  try {
+    const payload = req.body;
+    const timestamp = payload.head_commit?.timestamp?.slice(0, 10) || null;
+    const repoFullName = payload.repository?.full_name;
+    const commitSha = payload.after;
+    const githubSenderId = payload.sender?.id;
+    const pathList = payload?.head_commit?.added;
+    const platform = flatformMap[payload?.head_commit?.added[0].split("/")[0]];
 
-  req.timestamp = timestamp;
-  req.repoFullName = repoFullName;
-  req.githubSenderId = githubSenderId;
-  req.commitSha = commitSha;
-  req.pathList = pathList;
-  req.platform = platform;
-  next();
+    req.timestamp = timestamp;
+    req.repoFullName = repoFullName;
+    req.githubSenderId = githubSenderId;
+    req.commitSha = commitSha;
+    req.pathList = pathList;
+    req.platform = platform;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 githubController.savePendingData = async (req, res, next) => {
-  const timestamp = req.timestamp;
-  const repoFullName = req.repoFullName;
-  const githubSenderId = req.githubSenderId;
-  const commitSha = req.commitSha;
-  const pathList = req.pathList;
-  const platform = req.platform;
+  try {
+    const timestamp = req.timestamp;
+    const repoFullName = req.repoFullName;
+    const githubSenderId = req.githubSenderId;
+    const commitSha = req.commitSha;
+    const pathList = req.pathList;
+    const platform = req.platform;
 
-  const baseUrl = `https://raw.githubusercontent.com/${repoFullName}/${commitSha}/`;
-  const readmePath = pathList.find((p) => p.toLowerCase().endsWith(".md"));
-  const codePath = pathList.find((p) => !p.toLowerCase().endsWith(".md"));
+    const baseUrl = `https://raw.githubusercontent.com/${repoFullName}/${commitSha}/`;
+    const readmePath = pathList.find((p) => p.toLowerCase().endsWith(".md"));
+    const codePath = pathList.find((p) => !p.toLowerCase().endsWith(".md"));
 
-  const [readmd, code] = await Promise.all([
-    axios.get(baseUrl + readmePath).then((res) => res.data),
-    axios.get(baseUrl + codePath).then((res) => res.data),
-  ]);
+    const [readmd, code] = await Promise.all([
+      axios.get(baseUrl + readmePath).then((res) => res.data),
+      axios.get(baseUrl + codePath).then((res) => res.data),
+    ]);
 
-  let newPending;
-  if (platform == "beakjoon") {
-    newPending = parseBaekjoonReadme(readmd);
-  } else if (platform == "programmers") {
-    newPending = parseProgrammersReadme(readmd);
-  } else {
-    const error = new Error("Platform not found");
-    error.status = 404;
-    return next(error);
+    let newPending;
+    if (platform == "beakjoon") {
+      newPending = parseBaekjoonReadme(readmd);
+    } else if (platform == "programmers") {
+      newPending = parseProgrammersReadme(readmd);
+    } else {
+      const error = new Error("Platform not found");
+      error.status = 404;
+      return next(error);
+    }
+
+    newPending.code = code;
+    newPending.timestamp = timestamp;
+    newPending.state = "pending";
+    newPending.platform = platform;
+
+    const user = await User.findOne({ githubId: githubSenderId });
+
+    if (!user) {
+      const error = new Error("GitHub access token not found for this user");
+      error.status = 404;
+      return next(error);
+    }
+
+    newPending.userId = user._id;
+    await Problem.findOneAndUpdate(
+      {
+        userId: newPending.userId,
+        platform: newPending.platform,
+        problemId: newPending.problemId,
+      },
+      {
+        $set: {
+          code: newPending.code,
+          timestamp: newPending.timestamp,
+          state: "pending",
+          link: newPending.link,
+          memory: newPending.memory,
+          time: newPending.time,
+        },
+        $setOnInsert: {
+          title: newPending.title,
+          tier: newPending.tier,
+          userId: newPending.userId,
+          platform: newPending.platform,
+          problemId: newPending.problemId,
+          categories: newPending.categories,
+          description: newPending.description,
+        },
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    );
+
+    return res.status(201).json({ message: "Success receive webhook" });
+  } catch (error) {
+    next(error);
   }
-
-  newPending.code = code;
-  newPending.timestamp = timestamp;
-  newPending.state = "pending";
-  newPending.platform = platform;
-
-  const user = await User.findOne({ githubId: githubSenderId });
-
-  if (!user) {
-    const error = new Error("GitHub access token not found for this user");
-    error.status = 404;
-    return next(error);
-  }
-
-  newPending.userId = user._id;
-  await Problem.create(newPending);
 };
 
 module.exports = githubController;
